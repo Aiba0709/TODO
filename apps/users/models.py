c891bf43a567de7c2a0e122296e64fbb67cbf59a
Aiba_0709 <baimuratovaibek01@gmail.com>
2023-03-07 13:58:29 +0600

    users - Model create

---
+++ apps/users/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name= "Ваша почта",
+        unique=True
+    )
+    phone_number = models.CharField(
+        max_length=15,
+        verbose_name="ВАш тел.ном",
+
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    age = models.CharField(
+        max_length=2,
+        verbose_name="Ваш возраст"
+    )
+
+    def __str__(self):
+        return self.username
+    
+    class Mete:
+        verbose_name = "Пользователи",
+        verbose_name_plural = "Пользователь"
--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # rest
+    'rest_framework',
+    # apps
+    'apps.users',
+    'apps.todo',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +129,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
